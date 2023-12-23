10e42a45c45e2c9d10721c18ab6cc440f999d627
Matteo Doyen <matteo.doyen@etu.univ-tours.fr>
2023-12-23 14:18:21 +0100

    benchmark multi-thread vs mono

--- src/main.c
+++ src/main.c
@@ -12,27 +12,29 @@
 
 int main()
 {
-//    struct timespec start, end;
-//    double elapsed_time_mono;
-//    double elapsed_time_thread;
+   struct timespec start, end;
+   double elapsed_time_mono;
+   double elapsed_time_thread;
 
-    for (int i = 11; i < 12; i++)
+    for (int i = 10; i <= 20; i++)
     {
         selected_edge_t * selected_edges;
         // printf("budget : %d\n",i);
-        // clock_gettime(CLOCK_MONOTONIC, &start);
-        // get_edges_to_optimize_for_budget(i,GRAPH_FILE_NAME,PATHS_FILE_NAME);
-        // clock_gettime(CLOCK_MONOTONIC, &end);
-        // elapsed_time_mono = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1e9;
+        clock_gettime(CLOCK_MONOTONIC, &start);
+        get_edges_to_optimize_for_budget(i,GRAPH_FILE_NAME,PATHS_FILE_NAME,&selected_edges);
+        clock_gettime(CLOCK_MONOTONIC, &end);
+        free_select_edges(selected_edges);
+        elapsed_time_mono = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1e9;
 
-        // clock_gettime(CLOCK_MONOTONIC, &start);
-        get_edges_to_optimize_for_budget_threaded(10,GRAPH_FILE_NAME,PATHS_FILE_NAME,i,&selected_edges);
-        // clock_gettime(CLOCK_MONOTONIC, &end);
-        // elapsed_time_thread = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1e9;
-        // printf("%d %f\n",i,elapsed_time_thread);
-        // Calculate the elapsed time in seconds
+        clock_gettime(CLOCK_MONOTONIC, &start);
+        get_edges_to_optimize_for_budget_threaded(i,GRAPH_FILE_NAME,PATHS_FILE_NAME,12,&selected_edges);
+        print_selected_edges(selected_edges);
+        clock_gettime(CLOCK_MONOTONIC, &end);
+        elapsed_time_thread = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1e9;
+        printf("%d %f %f\n",i,elapsed_time_thread,elapsed_time_mono);
         // printf("\n");
-        free_select_edge(selected_edges);
+        // printf("%d\n",i);
+        free_select_edges(selected_edges);
     }
     
     // Print the result

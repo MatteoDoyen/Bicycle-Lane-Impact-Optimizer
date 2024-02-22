1543e6a2abd77b71a28cc99489c1f3a894fe1ff5
Matteo Doyen <matteo.doyen@etu.univ-tours.fr>
2024-02-22 17:25:37 +0100

    refactor

--- unit_test/main_test.c
+++ unit_test/main_test.c
@@ -20,39 +20,34 @@ void tearDown(void)
 int main(void)
 {
     UNITY_BEGIN();
-    // RUN_TEST(test_get_edges_to_optimize_for_budget_no_edge);
+    // dijkstra forward tests
     RUN_TEST(test_dijkstra_forward_one_vertex);
     RUN_TEST(test_dijkstra_forward_two_vertex);
     RUN_TEST(test_dijkstra_forward_straight_line);
     RUN_TEST(test_dijkstra_forward_complex_graph);
     RUN_TEST(test_dijkstra_forward_negative_edge);
     RUN_TEST(test_dijkstra_forward_no_path);
-
+    // dijstra backward tests
     RUN_TEST(test_dijkstra_backward_one_vertex);
     RUN_TEST(test_dijkstra_backward_two_vertex);
     RUN_TEST(test_dijkstra_backward_straight_line);
     RUN_TEST(test_dijkstra_backward_complex_graph);
     RUN_TEST(test_dijkstra_backward_negative_edge);
     RUN_TEST(test_dijkstra_backward_no_path);
-
-    // RUN_TEST(test_dijkstra_backward_straight_line);
-    // RUN_TEST(test_dijkstra_backward_complex_graph);
-    // RUN_TEST(test_dijkstra_backward_negative_edge);
-    // RUN_TEST(test_dijkstra_backward_no_path);
-
+    // min_distance tests
     RUN_TEST(test_min_distance_empty_dist_array_and_list);
     RUN_TEST(test_min_distance_empty_list);
     RUN_TEST(test_min_distance_high_value);
     RUN_TEST(test_min_distance_negative);
     RUN_TEST(test_min_distance_zero);
-
+    // vertices scaling tests
     RUN_TEST(test_vertices_in_array_scaling);
     RUN_TEST(test_vertices_out_array_scaling);
-
+    // cost function tests
     RUN_TEST(test_cost_function_alpha_equal_one);
     RUN_TEST(test_cost_function_alpha_equal_zero_point_five);
     RUN_TEST(test_cost_function_alpha_equal_zero);
-
+    // updated distance tests
     RUN_TEST(test_updated_dist_zero_distance);
     RUN_TEST(test_updated_dist_zero_danger);
     RUN_TEST(test_updated_dist_max_distance);
